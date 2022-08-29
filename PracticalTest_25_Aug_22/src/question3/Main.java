40d9dba12e07afee8ff2bc04759d4a3728dd3d25
utkar <utkar@Utkarsh>
2022-08-29 13:21:41 +0530

    test

--- PracticalTest_25_Aug_22/src/question3/Main.java
+++ PracticalTest_25_Aug_22/src/question3/Main.java
@@ -1,14 +1,14 @@
 package question3;
 
-public class Main<PostGraduate> {
+public class Main {
 	int count = 0;
-	void countObject(PostGraduate b1) {
+	void countObject(PostGaduate b1) {
 		((PostGaduate) b1).getDegree();
 		}
 	
 	public static void main(String[] args) {
 		Main countObj1 = new Main();
-		PostGraduate post = new PostGraduate();
+		PostGaduate post = new PostGaduate();
 		countObj1.countObject(post);
 	}
 
